9d1a6d176d34a1e5feb4a6c24319c4cf0d00f146
Shahabuddin Ansari <ansari.extc@gmail.com>
2025-01-13 22:52:38 +0800

    Initial commit: Basic SAP UI5 Todo List App

---
+++ webapp/Component.js
@@ -0,0 +1,15 @@
+sap.ui.define(["sap/ui/core/UIComponent"], (UIComponent) => {
+  "use strict";
+
+  return UIComponent.extend("sap.ui.demo.todo.Component", {
+    metadata: {
+      interfaces: ["sap.ui.core.IAsyncContentCreation"],
+      manifest: "json",
+    },
+    init() {
+      // Call the base component's init function
+      console.log("component initialized");
+      UIComponent.prototype.init.apply(this, arguments);
+    },
+  });
+});
