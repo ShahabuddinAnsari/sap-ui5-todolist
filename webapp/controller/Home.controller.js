322bf110b8f6ba1db9c5a7426d703ac3fd090477
Shahabuddin Ansari <ansari.extc@gmail.com>
2025-01-14 22:23:14 +0800

    modified:   webapp/Component.js 	modified:   webapp/controller/App.controller.js 	new file:   webapp/controller/BaseController.js 	new file:   webapp/controller/Home.controller.js 	modified:   webapp/i18n/i18n.properties 	modified:   webapp/manifest.json 	modified:   webapp/view/App.view.xml 	new file:   webapp/view/Home.view.xml

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,97 @@
+sap.ui.define(
+  [
+    "sap/ui/demo/todo/controller/BaseController",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast",
+  ],
+  (BaseController, JSONModel, MessageToast) => {
+    "use strict";
+
+    return BaseController.extend("sap.ui.demo.todo.controller.TaskList", {
+      onInit() {
+        const oData = {
+          tasks: [],
+        };
+
+        this.getView().setModel(new JSONModel(oData));
+      },
+
+      onAddTask() {
+        const taskInput = this.getView().byId("taskInput");
+        const inputValue = taskInput.getValue()?.trim();
+
+        if (!inputValue) return;
+
+        const oModel = this.getView().getModel();
+        const oData = oModel.getData();
+
+        oData.tasks.push({
+          title: inputValue,
+          isCompleted: false,
+          id: Math.floor(Math.random() * 100000000),
+        });
+
+        oModel.setData(oData);
+        taskInput.setValue("");
+      },
+
+      onTaskCheck(oEvent) {
+        const bSelected = oEvent.getParameter("selected");
+        const sTaskId = oEvent
+          .getSource()
+          .getBindingContext()
+          .getProperty("id");
+
+        const aTasks = this.getView().getModel().getProperty("/tasks");
+        const aTaskWithUpdatedState = aTasks.map((task) => {
+          return task.id === sTaskId
+            ? { ...task, isCompleted: bSelected }
+            : { ...task };
+        });
+
+        this.getView().getModel().setProperty("/tasks", aTaskWithUpdatedState);
+
+        const oCheckBox = oEvent.getSource();
+        const oHbox = oCheckBox.getParent();
+
+        const oText = oHbox.getItems()[1];
+        const oContext = oCheckBox.getBindingContext();
+
+        oContext.getProperty("isCompleted")
+          ? oText.addStyleClass("task-completed")
+          : oText.removeStyleClass("task-completed");
+      },
+
+      onDeleteTaskList(oEvent) {
+        const selectedListId = oEvent
+          .getParameter("listItem")
+          .getBindingContext()
+          .getProperty("id");
+
+        const sTaskMessage = oEvent
+          .getParameter("listItem")
+          .getBindingContext()
+          .getProperty("title");
+
+        const aTasks = this.getView().getModel().getProperty("/tasks");
+
+        const aUpdatedTasksList = aTasks.filter(
+          (task) => task.id !== selectedListId
+        );
+
+        this.getView().getModel().setProperty("/tasks", aUpdatedTasksList);
+
+        // Show Deleted task in the Message Toast
+        const sToastMsgDesc =
+          aUpdatedTasksList.length > 0
+            ? `Task: ${sTaskMessage} is deleted`
+            : "All Tasks are deleted now!";
+        this.handleMessageToast(sToastMsgDesc);
+      },
+
+      handleMessageToast(message) {
+        MessageToast.show(message);
+      },
+    });
+  }
+);
